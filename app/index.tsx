3ea47ca1fcfeda1debc4bc86d5efd978159e33cc
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-08-29 15:20:43 -0700

    initial

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Top-level page</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: '#1e212b',
+        white: '#F8F7F4',
+        sage: '#a9e5bb',
+        sagetxt: '#0FAB85',
+        yellow: '#f7b32b',
+        yellowtxt: '#F2A202',
+      },
+    },
+  },
+  plugins: [],
+}
